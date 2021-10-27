02967c164aa7ad0067cfc622739a5c50743310aa
shahad tariq <shahadtariq9@gmail.com>
2021-10-28 02:14:28 +0300

    addfun toData() in state class

--- app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
@@ -1,5 +1,27 @@
 package com.lemon.team.electronics.ui.productDetails
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.lemon.team.electronics.model.Repository
+import com.lemon.team.electronics.model.response.productById.ProductResponse
 import com.lemon.team.electronics.ui.base.BaseViewModel
+import com.lemon.team.electronics.util.State
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class ProductDetailsViewModel :BaseViewModel()
+class ProductDetailsViewModel : BaseViewModel() {
+
+    var detailsProduct = MutableLiveData<State<ProductResponse?>>()
+
+    fun getDetailsProduct() {
+        viewModelScope.launch {
+            Repository.getProductById("4720df27-ac48-4f90-8235-b443dc8d45c1")
+                .catch {  }
+                .collect {
+                    detailsProduct.value = it
+                }
+        }
+    }
+
+}
--- app/src/main/java/com/lemon/team/electronics/util/State.kt
+++ app/src/main/java/com/lemon/team/electronics/util/State.kt
@@ -6,4 +6,5 @@ sealed class State<out T> {
     data class Error(val message: String): State<Nothing>()
     object Loading : State<Nothing>()
 
+    fun toData(): T? = if (this is Success) data else null
 }
