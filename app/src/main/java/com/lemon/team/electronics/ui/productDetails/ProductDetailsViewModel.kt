bc8ad1a60d16431088af2a401db981925bb36c46
shahad tariq <shahadtariq9@gmail.com>
2021-10-28 20:37:15 +0300

    add data to fragment details

--- app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
@@ -1,27 +1,37 @@
 package com.lemon.team.electronics.ui.productDetails
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.model.response.productById.ProductResponse
 import com.lemon.team.electronics.ui.base.BaseViewModel
 import com.lemon.team.electronics.util.State
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 
 class ProductDetailsViewModel : BaseViewModel() {
 
     var detailsProduct = MutableLiveData<State<ProductResponse?>>()
 
-    fun getDetailsProduct() {
+    init {
+        getDetailsProduct()
+    }
+
+    private fun getDetailsProduct() {
         viewModelScope.launch {
             Repository.getProductById("4720df27-ac48-4f90-8235-b443dc8d45c1")
-                .catch {  }
+                .flowOn(Dispatchers.IO)
+                .catch { Log.i("hhhhhhhhhh" , "gggg") }
                 .collect {
-                    detailsProduct.value = it
+                    Log.i("hhhhhhhhhh" , it.toData().toString())
+                    detailsProduct.postValue(it)
                 }
         }
     }
+
 
 }
--- app/src/main/java/com/lemon/team/electronics/util/BindingAdapter.kt
+++ app/src/main/java/com/lemon/team/electronics/util/BindingAdapter.kt
@@ -1,2 +1,66 @@
 package com.lemon.team.electronics.util
 
+import android.text.Html
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
+import com.bumptech.glide.Glide
+
+
+@BindingAdapter(value = ["app:parseHtmlText"])
+fun parseHtml(view: TextView, text: String?) {
+    text?.let {
+        view.text = Html.fromHtml(text)
+    }
+}
+
+@BindingAdapter(value = ["app:urlForImage"])
+fun setImage(view: ImageView, url: String?) {
+    url?.let {
+        Glide.with(view)
+            .load(url)
+            .centerCrop()
+            .into(view)
+    }
+}
+
+@BindingAdapter(value = ["app:checkProduct"])
+fun checkStateProduct(view: View, value: Boolean) {
+    view.isVisible = !value
+}
+
+//
+//@BindingAdapter(value = ["value"])
+//fun setPikerNumber(view: HorizontalQuantitizer, value: Int?) {
+//
+//    view.setMinusIconColor(R.color.black)
+//    view.setMinusIconBackgroundColor(R.color.black)
+//    view.setPlusIconColor(R.color.black)
+//    view.setPlusIconBackgroundColor(R.color.white)
+//
+//    if (view.value != value){
+//        value?.let { view.value = it }
+//    }
+//}
+//
+//@InverseBindingAdapter(attribute = "value", event = "pikerNumberChangeEvent")
+//fun getPikerNumber(view: HorizontalQuantitizer): Int? {
+//    return view.value
+//}
+//
+//@BindingAdapter("pikerNumberChangeEvent")
+//fun setPikerListener(view: HorizontalQuantitizer, attChange: InverseBindingListener) {
+//    view.setQuantitizerListener(object : QuantitizerListener {
+//        override fun onDecrease() {
+//            attChange.onChange()
+//        }
+//        override fun onIncrease() {
+//            attChange.onChange()
+//        }
+//    })
+//}
+
