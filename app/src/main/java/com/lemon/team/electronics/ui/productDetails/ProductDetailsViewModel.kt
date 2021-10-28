d66816950660acf275f3cdbb4178e0275e5c694b
shahad tariq <shahadtariq9@gmail.com>
2021-10-28 20:57:54 +0300

    delete import Log

--- app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package com.lemon.team.electronics.ui.productDetails
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lemon.team.electronics.model.Repository
@@ -8,9 +7,7 @@ import com.lemon.team.electronics.model.response.productById.ProductResponse
 import com.lemon.team.electronics.ui.base.BaseViewModel
 import com.lemon.team.electronics.util.State
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class ProductDetailsViewModel : BaseViewModel() {
