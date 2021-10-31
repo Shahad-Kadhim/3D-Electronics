5d1426ef83fecdb8c4463f2e3f532221f11bb550
Shahad161 <shahad.hamza.014@gmail.com>
2021-10-31 16:04:40 +0300

    add back button

--- app/src/main/java/com/lemon/team/electronics/ui/about/AboutFragment.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/about/AboutFragment.kt
@@ -3,9 +3,13 @@ package com.lemon.team.electronics.ui.about
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.lemon.team.electronics.R
 import com.lemon.team.electronics.databinding.FragmentAboutBinding
 import com.lemon.team.electronics.ui.base.BaseFragment
+import com.lemon.team.electronics.ui.search.SearchFragmentDirections
+import com.lemon.team.electronics.util.EventObserver
+import com.lemon.team.electronics.util.goToFragment
 
 class AboutFragment:BaseFragment<FragmentAboutBinding,AboutViewModel>() {
     override val layoutId: Int = R.layout.fragment_about
@@ -26,7 +30,13 @@ class AboutFragment:BaseFragment<FragmentAboutBinding,AboutViewModel>() {
             this.otherCompaniesRecycler.adapter =
                 AboutAdapter(mutableListOf(),this@AboutFragment.viewModel)
         }
+        observeEvents()
+    }
 
+    private fun observeEvents(){
+        viewModel.clickBackEvent.observe(this, EventObserver{
+            findNavController().popBackStack()
+        })
     }
 
 }
--- app/src/main/java/com/lemon/team/electronics/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/about/AboutViewModel.kt
@@ -1,13 +1,23 @@
 package com.lemon.team.electronics.ui.about
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.ui.base.BaseInteractionListener
 import com.lemon.team.electronics.ui.base.BaseViewModel
+import com.lemon.team.electronics.util.Event
 
 class AboutViewModel :BaseViewModel(), BaseInteractionListener{
 
     var companies = Repository.getVendors()
     var otherCompanies = Repository.getOtherVendors()
 
+    private val _clickBackEvent = MutableLiveData<Event<Boolean>>()
+    var clickBackEvent : LiveData<Event<Boolean>> = _clickBackEvent
+
     var video = "https://www.3d-iraq.com/static/media/vedio-3d.146c63e2.mp4"
+
+    fun onclickBack(){
+        _clickBackEvent.postValue(Event(true))
+    }
 }
