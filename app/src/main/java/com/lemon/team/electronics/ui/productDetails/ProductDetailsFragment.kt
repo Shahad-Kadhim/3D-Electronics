05a1840d60953b4c5eb53c3f68904532d0e42541
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-11-01 16:39:37 +0300

    show info of product on ui and enable buttons

--- app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsFragment.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsFragment.kt
@@ -4,24 +4,53 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.lemon.team.electronics.R
 import com.lemon.team.electronics.databinding.FragmentProductDetailsBinding
 import com.lemon.team.electronics.ui.base.BaseFragment
+import com.lemon.team.electronics.util.EventObserver
 
 class ProductDetailsFragment :
     BaseFragment<FragmentProductDetailsBinding, ProductDetailsViewModel>() {
 
     override val layoutId: Int = R.layout.fragment_product_details
     override val viewModel: ProductDetailsViewModel by viewModels()
-
+    private val args :ProductDetailsFragmentArgs by navArgs()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) ->
     FragmentProductDetailsBinding = DataBindingUtil::inflate
 
     override fun setUp() {
-        binding?.apply {
+        binding.apply {
             this.lifecycleOwner = viewLifecycleOwner
             this.viewModel = this@ProductDetailsFragment.viewModel
+            productImages.adapter=ProductImageAdapter(emptyList(),this@ProductDetailsFragment.viewModel)
         }
+        viewModel.getDetailsProduct(args.productId)
+        observeEvents()
+    }
+
+    private fun observeEvents() {
+
+        viewModel.onclickAddToCart.observe(this,EventObserver{
+            // add to cart table when create database
+            1
+        })
+
+        viewModel.onclickWish.observe(this,EventObserver{
+            // add to wish table when create database
+            1
+        })
+
+        viewModel.onclickShare.observe(this,EventObserver{
+            // add code to share link of this product
+            1
+        })
+
+        viewModel.onclickBack.observe(this,EventObserver{
+            findNavController().popBackStack()
+        })
+
     }
 
 }
--- app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/productDetails/ProductDetailsViewModel.kt
@@ -1,10 +1,10 @@
 package com.lemon.team.electronics.ui.productDetails
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.model.response.productById.ProductResponse
 import com.lemon.team.electronics.ui.base.BaseViewModel
+import com.lemon.team.electronics.util.Event
 import com.lemon.team.electronics.util.State
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -12,9 +12,40 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 
-class ProductDetailsViewModel : BaseViewModel() {
+class ProductDetailsViewModel : BaseViewModel(),ImageInteractionListener {
 
-    var detailsProduct = MutableLiveData<State<ProductResponse?>>()
+    private var _detailsProduct = MutableLiveData<State<ProductResponse?>>()
+    val detailsProduct :LiveData<State<ProductResponse?>> =_detailsProduct
+
+    val images =Transformations.map(detailsProduct){
+        it.toData()?.imageJsonResponses?.map { it.productImageLocation }
+    }
+    val mainImage= MediatorLiveData<String>().apply {
+        addSource(detailsProduct){
+            if(it is State.Success){
+                it.data?.picLocation?.let { url ->
+                    updateMainImage(url)
+                }
+            }
+        }
+    }
+
+    private var _onclickBack =MutableLiveData<Event<Boolean>>()
+    val onclickBack :LiveData<Event<Boolean>> = _onclickBack
+
+    private var _onclickShare =MutableLiveData<Event<String>>()
+    val onclickShare :LiveData<Event<String>> = _onclickShare
+
+    private var _onclickWish =MutableLiveData<Event<String>>()
+    val onclickWish :LiveData<Event<String>> = _onclickWish
+
+    private var _onclickAddToCart =MutableLiveData<Event<String>>()
+    val onclickAddToCart :LiveData<Event<String>> = _onclickAddToCart
+
+
+    private fun updateMainImage(url:String){
+        mainImage.postValue(url)
+    }
 
     fun getDetailsProduct(productId: String) {
         viewModelScope.launch {
@@ -22,9 +53,30 @@ class ProductDetailsViewModel : BaseViewModel() {
                 .flowOn(Dispatchers.IO)
                 .catch { }
                 .collect {
-                    detailsProduct.postValue(it)
+                    _detailsProduct.postValue(it)
                 }
         }
     }
+
+    fun onclickBack(){
+        _onclickBack.postValue(Event(true))
+    }
+
+    fun onclickWish(productId: String){
+        _onclickWish.postValue(Event(productId))
+    }
+
+    fun onclickAddToCart(productId: String){
+        _onclickAddToCart.postValue(Event(productId))
+    }
+
+    fun onclickShare(link:String){
+        _onclickShare.postValue(Event(link))
+    }
+
+    override fun onclickImage(url: String) {
+        mainImage.postValue(url)
+    }
+
 
 }
