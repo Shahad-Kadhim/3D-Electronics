87820b8abc58d4fc2b14c2109b1f43020dfdcfdf
Shahad161 <shahad.hamza.014@gmail.com>
2021-11-03 22:46:31 +0300

    fix: rename classes and property and delete duplication classes

--- app/src/main/java/com/lemon/team/electronics/ui/home/HomeItem.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/home/HomeItem.kt
@@ -1,15 +1,16 @@
 package com.lemon.team.electronics.ui.home
 
-import com.lemon.team.electronics.model.response.Content
-import com.lemon.team.electronics.model.response.categories.CategoriesResponseItem
+import com.lemon.team.electronics.model.response.Product
+import com.lemon.team.electronics.model.response.categories.CategoryResponse
 import com.lemon.team.electronics.model.response.productsByCategoryId.CategoryInfoType
 
 
 sealed class HomeItem(val rank: Int) {
-    class SlideType(val items: List<Content>) : HomeItem(0)
+    class SlideType(val items: List<Product>) : HomeItem(0)
     class SearchType : HomeItem(1)
-    class CategoriesType(val items: List<CategoriesResponseItem>) : HomeItem(2)
-    class BestProductType(val items: List<Content>) : HomeItem(3)
+    class CategoriesType(val items: List<CategoryResponse>) : HomeItem(2)
+    class BestProductType(val items: List<Product>) : HomeItem(3)
     class ElementCategoriesType(
-        val items: List<Content>, val categoryInfo: CategoryInfoType) : HomeItem(4)
+        val items: List<Product>, val categoryInfo: CategoryInfoType
+    ) : HomeItem(4)
 }
