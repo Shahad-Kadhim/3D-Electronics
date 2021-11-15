da26e65b1fa73dfc07a8c29d3070a00ce7ab8dff
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-11-15 17:12:19 +0300

    connect views with view model

--- app/src/main/java/com/lemon/team/electronics/ui/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/checkout/CheckoutViewModel.kt
@@ -1,5 +1,27 @@
 package com.lemon.team.electronics.ui.checkout
 
+import androidx.lifecycle.*
+import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.ui.base.*
+import com.lemon.team.electronics.util.*
 
-class CheckoutViewModel : BaseViewModel(), BaseInteractionListener
+class CheckoutViewModel : BaseViewModel(), BaseInteractionListener {
+
+    val products = Repository.getProductsInCart().asLiveData()
+
+    val numberOfProducts = Transformations.map(products) {
+        if (it is State.Success) {
+            it.data?.products?.size
+        } else {
+            0
+        }
+    }
+
+    val totalPrice: LiveData<Double> =
+        Transformations.map(products) { state ->
+            state.toData()?.products?.map { it.price }?.sum()
+        }
+
+}
+
+
--- app/src/main/java/com/lemon/team/electronics/util/Extension.kt
+++ app/src/main/java/com/lemon/team/electronics/util/Extension.kt
@@ -3,8 +3,10 @@ package com.lemon.team.electronics.util
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import androidx.lifecycle.*
-import androidx.navigation.*
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.FragmentNavigator
 import com.lemon.team.electronics.BR
 import com.lemon.team.electronics.R
@@ -16,8 +18,8 @@ fun View.goToFragment(navDir: NavDirections) {
     Navigation.findNavController(this).navigate(navDir)
 }
 
-fun View.goToFragmentWithTransition(navDir: NavDirections,extra: FragmentNavigator.Extras) {
-    Navigation.findNavController(this).navigate(navDir,extra)
+fun View.goToFragmentWithTransition(navDir: NavDirections, extra: FragmentNavigator.Extras) {
+    Navigation.findNavController(this).navigate(navDir, extra)
 }
 
 fun Context.readJsonAsset(fileName: String): String? {
@@ -35,17 +37,17 @@ fun BaseRecyclerAdapter.ItemViewHolder.setVariableAdapter(item: Any?) {
 }
 
 
-fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, f:(T) ->Unit){
-    this.observe(owner, EventObserver{
+fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, f: (T) -> Unit) {
+    this.observe(owner, EventObserver {
         f(it)
     })
 }
 
 fun Intent.sharingUrl(url: String?): Intent? {
-    return  this.apply {
+    return this.apply {
         type = "text/plain"
         putExtra(Intent.EXTRA_SUBJECT, "product link")
-        putExtra(Intent.EXTRA_TEXT, Constants.URL_PRODUCT_WEBSITE +url)
+        putExtra(Intent.EXTRA_TEXT, Constants.URL_PRODUCT_WEBSITE + url)
     }
 }
 
@@ -54,7 +56,7 @@ fun <T> getSixItems(itemList: List<T>) = itemList.take(6)
 
 
 fun String.convertToCategoryIcon(): Int =
-    when(this){
+    when (this) {
         CategoriesId.MIC -> R.drawable.ic_mic
         CategoriesId.ADAPTER -> R.drawable.ic_cable
         CategoriesId.CASE -> R.drawable.ic_case_icon
@@ -62,18 +64,22 @@ fun String.convertToCategoryIcon(): Int =
         CategoriesId.CPU -> R.drawable.ic_cpu
         CategoriesId.GRAPHICS_CARD -> R.drawable.ic_graphics_card
         CategoriesId.LAPTOP_FAN,
-        CategoriesId.CASE_FAN-> R.drawable.ic_fan
+        CategoriesId.CASE_FAN,
+        -> R.drawable.ic_fan
         CategoriesId.SIMULATORS -> R.drawable.ic_game
-        CategoriesId.HEADSETS ,
-        CategoriesId.REPLACEMENT_EAR_PADS-> R.drawable.ic_headphone
+        CategoriesId.HEADSETS,
+        CategoriesId.REPLACEMENT_EAR_PADS,
+        -> R.drawable.ic_headphone
         CategoriesId.STORAGE_AND_MEMORY,
-        CategoriesId.EXTERNAL_HARD-> R.drawable.ic_storage
-        CategoriesId.GAMING_CHAIRS-> R.drawable.ic_chairs
+        CategoriesId.EXTERNAL_HARD,
+        -> R.drawable.ic_storage
+        CategoriesId.GAMING_CHAIRS -> R.drawable.ic_chairs
         CategoriesId.LAPTOP -> R.drawable.ic_laptop
         CategoriesId.WEBCAM -> R.drawable.ic_webcam
         CategoriesId.MONITOR_STAND -> R.drawable.ic_stand
-        CategoriesId.POWER_PRODUCTS ,
-        CategoriesId.POWER_SUPPLY -> R.drawable.ic_power_supply
+        CategoriesId.POWER_PRODUCTS,
+        CategoriesId.POWER_SUPPLY,
+        -> R.drawable.ic_power_supply
         CategoriesId.FLASH_DRIVES -> R.drawable.ic_memory
         CategoriesId.MOUSE -> R.drawable.ic_mouse
         CategoriesId.KEYBOARD -> R.drawable.ic_keyboard
@@ -82,4 +88,11 @@ fun String.convertToCategoryIcon(): Int =
         CategoriesId.TABLET_AND_MOBILES -> R.drawable.ic_mobile
         CategoriesId.MONITORS -> R.drawable.ic_monitor
         else -> R.drawable.ic_pc
-}
+    }
+
+fun List<Double?>.sum(): Double =
+    this.filterNotNull().fold(0.0) { totalPrice, item ->
+        totalPrice + item
+    }
+
+
