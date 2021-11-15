be5f21bbb894501485905987112cd7cd10b47277
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-16 01:59:19 +0300

    init spinner

--- app/src/main/java/com/lemon/team/electronics/ui/customerInformation/CustomerInformationFragment.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/customerInformation/CustomerInformationFragment.kt
@@ -1,5 +1,6 @@
 package com.lemon.team.electronics.ui.customerInformation
 
+import android.widget.ArrayAdapter
 import com.lemon.team.electronics.R
 import com.lemon.team.electronics.databinding.FragmentCustomerInformationBinding
 import com.lemon.team.electronics.ui.base.BaseFragment
@@ -15,6 +16,15 @@ class CustomerInformationFragment :
     }
 
     override fun setUpBinding() {
+        initSpinner()
+    }
 
+    private fun initSpinner() {
+        val itemList = context?.resources?.getStringArray(R.array.city)
+        val spinnerAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item, itemList!!)
+        binding.governorateInput.apply {
+            adapter = spinnerAdapter
+            onItemSelectedListener = viewModel
+        }
     }
 }
