702476c06d87111ac384685ee1200d410ac0b963
Bareq <bareqaltaamah@gmail.com>
2021-11-20 21:14:06 +0300

    add version to about page

--- app/src/main/java/com/lemon/team/electronics/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/about/AboutViewModel.kt
@@ -2,6 +2,7 @@ package com.lemon.team.electronics.ui.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.lemon.team.electronics.BuildConfig
 import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.ui.base.*
 import com.lemon.team.electronics.util.*
@@ -11,6 +12,8 @@ class AboutViewModel :BaseViewModel(), BaseInteractionListener{
 
     var companies = Repository.getCompanies()
     var otherCompanies = Repository.getOtherCompanies()
+
+    val appVersion = BuildConfig.VERSION_NAME
 
     private val _clickBackEvent = MutableLiveData<Event<Boolean>>()
     var clickBackEvent : LiveData<Event<Boolean>> = _clickBackEvent
