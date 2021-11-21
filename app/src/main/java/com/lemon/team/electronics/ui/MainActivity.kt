6f14a33f05c4eb89af9febc46b106acbaded634c
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-21 12:27:30 +0300

    feat: add splash screen

--- app/src/main/java/com/lemon/team/electronics/ui/MainActivity.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_3DElectronics)
         binding=DataBindingUtil.setContentView<ActivityMainBinding>(
             this,
             R.layout.activity_main
