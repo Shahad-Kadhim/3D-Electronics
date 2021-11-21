364db4d8c6c66e143a09f6cc86904ca047ecdd4a
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-11-21 10:12:43 +0300

    add interaction back button

--- app/src/main/java/com/lemon/team/electronics/ui/orderTracking/OrderTrackingFragment.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/orderTracking/OrderTrackingFragment.kt
@@ -1,9 +1,10 @@
 package com.lemon.team.electronics.ui.orderTracking
 
+import androidx.navigation.fragment.findNavController
 import com.lemon.team.electronics.R
 import com.lemon.team.electronics.databinding.FragmentOrderTrackingBinding
 import com.lemon.team.electronics.ui.base.BaseFragment
-import com.lemon.team.electronics.ui.cart.CartRecyclerAdapter
+import com.lemon.team.electronics.util.observeEvent
 
 class OrderTrackingFragment: BaseFragment<FragmentOrderTrackingBinding, OrderTrackingViewModel>() {
 
@@ -19,6 +20,9 @@ class OrderTrackingFragment: BaseFragment<FragmentOrderTrackingBinding, OrderTra
     }
 
     override fun observeEvents() {
+        viewModel.clickBackEvent.observeEvent(this) {
+            findNavController().navigateUp()
+        }
 
     }
 }
--- app/src/main/java/com/lemon/team/electronics/ui/orderTracking/OrderTrackingViewModel.kt
+++ app/src/main/java/com/lemon/team/electronics/ui/orderTracking/OrderTrackingViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.lemon.team.electronics.model.Repository
 import com.lemon.team.electronics.model.orderTracking.OrderTrackingResponse
 import com.lemon.team.electronics.ui.base.BaseViewModel
+import com.lemon.team.electronics.util.Event
 import com.lemon.team.electronics.util.State
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -16,15 +17,22 @@ class OrderTrackingViewModel: BaseViewModel(), OrderInteractionListener {
     val phoneNumber = MutableLiveData<String>()
 
     private val _orderTrackingResponse = MutableLiveData<State<List<OrderTrackingResponse>?>>()
-    val orderTrackingResponse: LiveData<State<List<OrderTrackingResponse>?>> = _orderTrackingResponse
+    val orderTrackingResponse: LiveData<State<List<OrderTrackingResponse>?>> =
+        _orderTrackingResponse
+
+    private val _clickBackEvent = MutableLiveData<Event<Boolean>>()
+    var clickBackEvent: LiveData<Event<Boolean>> = _clickBackEvent
 
     fun trackOrder() {
         viewModelScope.launch {
-            Repository.trackOrder(phoneNumber.value).collect { orderTrackingResponse->
+            Repository.trackOrder(phoneNumber.value).collect { orderTrackingResponse ->
                 _orderTrackingResponse.postValue(orderTrackingResponse)
             }
         }
-        Log.i("kkk", _orderTrackingResponse.value.toString())
+    }
+
+    fun onClickBack() {
+        _clickBackEvent.postValue(Event(true))
     }
 
 }
